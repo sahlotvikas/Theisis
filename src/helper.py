a162e2f65e0434d0f7cd30a8e5b8355720230361
sahlotvikas <sahlotvikas@gmail.com>
2017-12-02 13:20:21 +0530

    Function for tokenization

--- src/helper.py
+++ src/helper.py
@@ -5,6 +5,7 @@ def word(sentence):
     sentence = input("Enter text")
     words = nltk.word_tokenize(sentence)
     print(words)
+<<<<<<< Updated upstream
 #return words
 
 word(" ")
@@ -31,33 +32,31 @@ def vocab():
 def pairs():
     uniques = []
     text = input("Enter text")
-    words[] = text.split(' ')
+    words = text.split(' ')
     for each in words:
         if each not in uniques:
             uniques.append(each)
     print(uniques)
-    
+
     frequency = []
-    for i in uniques:
-        frequency.append(uniques.count(i))
-    print(str(zip(uniques, frequency)))
-        
-def pairs():
-    
-    
-def dicts():
-    uniques = []
-    text = input("Enter text")
-    words[] = text.split(' ')
-    for each in words:
-        if each not in uniques:
-            uniques.append(each)
-    print(uniques)
-    dict_key = []
-    dict_word = []
-    for i in uniques:
-        dict_key = [i, uniques[i]]
-        dict_word = [uniques[i], i]
-        
-        print(dict_key)
-        print(dict_word)
+    for i in words:
+        frequency.append(words.count(i))
+    print(list(zip(words, frequency)))
+
+
+uniques = []
+text = input("Enter text")
+words = text.split(' ')
+for each in words:
+    if each not in uniques:
+        uniques.append(each)
+print("Text: ", uniques)
+
+sorted_uniques = sorted(list(set(uniques)))
+print("Sorted text: ", sorted_uniques)
+
+char_to_int = dict((c, i) for i, c in enumerate(uniques))
+print("Words to Integer Mapping: ", char_to_int)
+
+int_to_char = dict((i, c) for i, c in enumerate(uniques))
+print("Integer to Words Mapping:", int_to_char)
